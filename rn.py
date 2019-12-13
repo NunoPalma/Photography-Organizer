8b4403953f45d49fdae3b577d3d7ef4ee11c8401
NunoPalma <nunopalma98@gmail.com>
2019-12-13 04:37:21 +0000

    Added a rename sketch

--- rn.py
+++ rn.py
@@ -1,14 +1,39 @@
-import argparse
+import argparse 
+import os 
+parser = argparse.ArgumentParser()
+directory = os.getcwd() 
 
-parser = argparse.ArgumentParser(
-	description='Testing')
+def rename(directory=directory): 
+	i = 0
+	for file in os.listdir(directory):
+		file_path = directory + file
 
-def addArgs():
-	parser.add_argument('-a', action='store_true', default=False)
+		if not validateFile(file_path):
+			continue
 
-def parseArgs():
-	print(parser.parse_args(['-a']))
+		extension = os.path.splitext(file)[1] #There might be a better way to do this
+		new_file_name = directory + str(i) + extension
+		i += 1
+		os.rename(file_path, new_file_name)
 
-if __name__ == "__main__":
+def validateFile(file_path):
+	if os.path.isfile(file_path):
+		if file_path.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp')): #Figure out if the user should specify the extensions
+			return True
+	return False
+
+def addArgs(): 
+	parser.add_argument('-d', action='store')
+
+def parseArgs(args):
+	if args.d:
+		if not os.path.isdir(args.d):
+			print("Invalid directory - " + args.d)
+			exit(1)
+			
+if __name__ == "__main__": 
 	addArgs()
-	parseArgs()
+	args = parser.parse_args()
+	parseArgs(args)
+	rename(args.d)
+	#rename()
